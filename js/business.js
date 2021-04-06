fd6f1f47407b3e5f7f0a728f1a6d511379c65b83
couchpotatodev <mjrimayaho@gmail.com>
2021-04-06 20:37:35 +0800

    json file

--- js/business.js
+++ js/business.js
@@ -1,2 +1,20 @@
-const requestURL = '../chamber of commerce/js/business.json';
-var baguioBiz = document.querySelector('.prestoncard');
+const requestURL = 'json/business.json';
+
+/*const requestURL = 'https://github.com/couchpotatodev/chamber-of-commerce/blob/main/json/business.json';
+var baguioBiz = document.querySelector('.cards');
+*/
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const biz = jsonObject['business']; 
+
+
+
+
+    
+
+});
